ed738789306ecce0fb11d25a53fd2f17d6100bab
Mia Chen <mia89926@gmail.com>
2020-05-15 13:32:36 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -95,6 +95,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// DELETE 刪除餐廳
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
     console.log(`App is running on http://localhost:${port}`)
 })
