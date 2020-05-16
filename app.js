14f8858d4a3826abca5731977d9bc80a1bfe0d49
Mia Chen <mia89926@gmail.com>
2020-05-16 09:53:09 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const Restaurant = require('./models/restaurant')
 const methodOverride = require('method-override')
+const routes = require('./routes')
 const app = express()
 const port = 3000
 
@@ -29,82 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 // routes setting
-// READ 瀏覽所有餐廳
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
-})
-
-// CREATE 新增餐廳
-app.get('/restaurants/new', (req,res) => {
-  return res.render('new')
-})
-
-app.post('/restaurants', (req, res) => {
-  const restaurant = req.body
-  return Restaurant.create({
-    name: restaurant.name,
-    name: restaurant.name_en,
-    category: restaurant.category,
-    image: restaurant.image,
-    location: restaurant.location,
-    phone: restaurant.phone,
-    google_map: `https://www.google.com/maps/search/?api=1&query=${restaurant.location}`,
-    description: restaurant.description,
-    rating: restaurant.rating
-  })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-// READ 瀏覽特定餐廳詳細資訊
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-// UPDATE 編輯餐廳資料
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const restaurant = req.body
-  return Restaurant.findById(id)
-    .then(r => {
-      r.name = restaurant.name
-      r.name_en = restaurant.name_en
-      r.category = restaurant.category
-      r.rating = restaurant.rating
-      r.image = restaurant.image
-      r.location = restaurant.location
-      r.phone = restaurant.phone
-      r.google_map = `https://www.google.com/maps/search/?api=1&query=${restaurant.location}`
-      r.description = restaurant.description
-      return r.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
-
-// DELETE 刪除餐廳
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`App is running on http://localhost:${port}`)
---
+++ routes/modules/restaurants.js
@@ -0,0 +1,77 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../../models/restaurant')
+
+
+// CREATE 新增餐廳
+router.get('/new', (req,res) => {
+    return res.render('new')
+  })
+  
+router.post('/', (req, res) => {
+  const restaurant = req.body
+  return Restaurant.create({
+    name: restaurant.name,
+    name: restaurant.name_en,
+    category: restaurant.category,
+    image: restaurant.image,
+    location: restaurant.location,
+    phone: restaurant.phone,
+    google_map: `https://www.google.com/maps/search/?api=1&query=${restaurant.location}`,
+    description: restaurant.description,
+    rating: restaurant.rating
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+  
+// READ 瀏覽特定餐廳詳細資訊
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
+  
+// UPDATE 編輯餐廳資料
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+  
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, name_en, category, rating, image, location, phone, description} = req.body
+  return Restaurant.findById(id)
+    .then(r => {
+      r.name = name
+      r.name_en = name_en
+      r.category = category
+      r.rating = rating
+      r.image = image
+      r.location = location
+      r.phone = phone
+      r.google_map = `https://www.google.com/maps/search/?api=1&query=${location}`
+      r.description = description
+      return r.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+  
+// DELETE 刪除餐廳
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// 匯出路由模組
+module.exports = router
